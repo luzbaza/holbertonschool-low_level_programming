054ad191e56b21c1b315fd2690a3fbf3284d3e6b
luzbaza <2459@holbertonschool.com>
2021-02-05 23:46:12 -0500

    Numbersz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ *Return: numbers
+ */
+int main(void)
+{
+	int c;
+
+	for  (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
