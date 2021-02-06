f1fbc2c5cba1fe0a7d8276988706da95caf87727
luzbaza <2459@holbertonschool.com>
2021-02-05 23:18:28 -0500

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ *Return: numbers
+ */
+int main(void)
+{
+	char t;
+
+	for (t = 0; t < 10; t++)
+		printf("%d", t);
+	printf("\n");
+	return (0);
+}
