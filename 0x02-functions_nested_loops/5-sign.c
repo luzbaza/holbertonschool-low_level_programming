f69374d3384390c4ea679f76e8e4a7529191bf7e
luzbaza <2459@holbertonschool.com>
2021-02-09 22:08:07 -0500

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,6 +5,7 @@
  * Return: 1 print
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
@@ -13,7 +14,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar (0);
+		_putchar ('0');
 		return (0);
 	}
 	else
