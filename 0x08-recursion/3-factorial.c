ee5f2a4c53027bdfdfe99a34999eec8de9fe91a5
luzbaza <2459@holbertonschool.com>
2021-02-24 23:34:35 -0500

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * factorial - that returns the factorial of a given number
+ * @n: string
+ * Return: Always 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
