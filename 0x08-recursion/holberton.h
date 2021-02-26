2682a11ea2578abca5871eb8d92fd502879f9850
luzbaza <2459@holbertonschool.com>
2021-02-25 21:31:28 -0500

    holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,4 +1,6 @@
-int squareroot(int n, int c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -9,3 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif /*HOLBERTON_H */
