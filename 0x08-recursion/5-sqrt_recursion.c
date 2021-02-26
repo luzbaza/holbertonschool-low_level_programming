37a36ec645031aec376afcd85b0129a97dd46f18
luzbaza <2459@holbertonschool.com>
2021-02-25 21:24:49 -0500

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+int squareroot(int n, int c);
+/**
+ * _sqrt_recursion - that returns the natural square root of a number
+ * @n: string
+ * Return: Always 0
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (squareroot(n, (n + 1) / 2));
+}
+/**
+ * squareroot - check if perfect square
+ * @n: number being squared
+ * @c: counter
+ * Return: square root of the number
+ */
+int squareroot(int n, int c)
+{
+	if (c < 1)
+		return (-1);
+	else if (c * c == n)
+		return (c);
+	else
+		return (squareroot(n, c - 1));
+}
