ecffb86008b7ceff60dbda367b115d66a24e68e8
luzbaza <2459@holbertonschool.com>
2021-03-02 22:02:16 -0500

    holberton.h

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,5 @@
-#ifdef HOLBERTON_H
-#define HOLBERTON_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -7,6 +7,6 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-int _putchar(char c)
+int _putchar(char c);
 	
-#endif /*HOLBERTON_H */
+#endif  /* HOLBERTON_H */
