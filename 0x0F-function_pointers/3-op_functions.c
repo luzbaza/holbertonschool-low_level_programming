f5d0c434a8a3e9c71395af93bc3b108ee1e5ec18
luzbaza <2459@holbertonschool.com>
2021-03-10 23:13:45 -0500

    op

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - functions
+ * @a: one
+ * @b: two
+ * Return : Always 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - functions
+ * @a: one
+ * @b: two
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - functions
+ * @a: one
+ * @b: two
+ * Return: Always 0
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - functions
+ * @a: one
+ * @b: two
+ * Return: Always 0
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - functions
+ * @a: one
+ * @b: two
+ * Return: Always 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
