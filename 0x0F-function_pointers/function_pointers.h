b1b89af06a51da9604fb9a3d5cb6a1478dd6f3a6
luzbaza <2459@holbertonschool.com>
2021-03-10 21:21:30 -0500

    function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
