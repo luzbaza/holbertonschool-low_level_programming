1aae0b07a2160cce2db552864d229cceb9ec88a0
luzbaza <2459@holbertonschool.com>
2021-03-22 21:39:10 -0500

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,20 +13,14 @@ size_t print_list(const list_t *h)
 
 	if (h == NULL)
 		return (0);
-
-	while (h != NULL)
-
+	for (count = 0; h != NULL; count++)
 	{
 		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
+			printf("[0] (nil)\n");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
-		count++;
 	}
+
 	return (count);
 }
