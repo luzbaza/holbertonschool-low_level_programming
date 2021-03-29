ff469f6c0110d71e9c9d6451647ad1343cb4ef50
luzbaza <2459@holbertonschool.com>
2021-03-29 16:48:02 -0500

    101

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function that returns the number of bits
+ * you would need to flip to get from one number to another.
+ * @n: first number
+ * @m: second number
+ * Return: Always 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff, check;
+	unsigned int count, i;
+
+	count = 0;
+	check = 1;
+	diff = n ^ m;
+
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (check == (diff & check))
+			count++;
+		check <<= 1;
+	}
+	return (count);
+}
