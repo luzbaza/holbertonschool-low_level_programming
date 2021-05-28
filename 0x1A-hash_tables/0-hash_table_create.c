21b73c98bfaec3bf828e7542cf86ecdadd36b734
root <root@DESKTOP-VSC90TH.localdomain>
2021-05-28 16:43:43 -0500

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int idx = 0;
-	hash_table_t *new_table = NULL;
+	hash_table_t *new_table;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
