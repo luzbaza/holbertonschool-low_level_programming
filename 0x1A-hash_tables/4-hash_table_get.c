1c6e4aad2f9581e8fabc73f52faff157bb17cd90
root <root@DESKTOP-VSC90TH.localdomain>
2021-05-28 23:52:22 -0500

    >>> ht['betty']

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0;
+	hash_node_t *temp = NULL;
+
+	if (key == NULL)
+		return (NULL);
+
+	if (ht != NULL)
+	{
+		idx = key_index((const unsigned char *)key, ht->size);
+		temp = ht->array[idx];
+
+		while (temp != NULL)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
+}
