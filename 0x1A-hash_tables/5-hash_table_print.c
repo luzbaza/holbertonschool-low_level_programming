55f541454c759ba4d4e3df2d0fe2d7af4b08ac2e
root <root@DESKTOP-VSC90TH.localdomain>
2021-06-03 10:12:55 -0500

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_print - function that prints a hash table
  * @ht:  is the hash table
- * Return: hast table
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
