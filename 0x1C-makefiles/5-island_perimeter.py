5f6dc5efbf6ee47acfcbfb184c37835d78f073b7
luzbaza <2459@holbertonschool.com>
2021-07-07 16:44:49 -0500

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """  Island Perimeter """
 
-def island_perimeter(grid):
-	perimeter = 0
 
-	for row in range(0, len(grid)):
-		for zone in range(0, len(grid[row])):
-			if grid[row][zone] == 1:
-				if zone < len(grid[row]) - 1 and grid[row][zone + 1] == 0:
-					perimeter = perimeter + 1
-				if zone > 0 and grid[row][zone - 1] == 0:
-					perimeter = perimeter + 1
-				if row > 0 and grid[row - 1][zone] == 0:
-					perimeter = perimeter + 1
-				if row < len(grid) - 1 and grid[row + 1][zone] == 0:
-					perimeter = perimeter + 1
-				if row == 0 or row == len(grid) - 1:
-					perimeter = perimeter + 1
-				if zone == 0 or zone == len(grid[row]) - 1:
-					perimeter = perimeter + 1
-	return perimeter
+def island_perimeter(grid):
+    """ return de perimeter """
+    perimeter = 0
+
+    for row in range(0, len(grid)):
+        for zone in range(0, len(grid[row])):
+            if grid[row][zone] == 1:
+                if zone < len(grid[row]) - 1 and grid[row][zone + 1] == 0:
+                    perimeter = perimeter + 1
+                if zone > 0 and grid[row][zone - 1] == 0:
+                    perimeter = perimeter + 1
+                if row > 0 and grid[row - 1][zone] == 0:
+                    perimeter = perimeter + 1
+                if row < len(grid) - 1 and grid[row + 1][zone] == 0:
+                    perimeter = perimeter + 1
+                if row == 0 or row == len(grid) - 1:
+                    perimeter = perimeter + 1
+                if zone == 0 or zone == len(grid[row]) - 1:
+                    perimeter = perimeter + 1
+    return perimeter
